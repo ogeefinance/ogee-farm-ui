a109ef6cfb07a25f18f870f5a6331a647af1b985
SolProc <info@cheeseswap.app>
2021-04-06 01:47:59 +0600

    hooks, layout, apy calc updated

--- src/config/constants/ifo.ts
+++ src/config/constants/ifo.ts
@@ -2,24 +2,24 @@ import { Ifo } from './types'
 
 const ifos: Ifo[] = [
   {
-    id: 'OYT',
-    address: '',
+    id: 'oyt',
+    address: '0x247A7f073CE405cd1cAEB048b16A314997309ebD',
     isActive: true,
     name: 'Ogee Yield Token (OYT)',
     subTitle: 'The Best AMM and yield farm on Huobi ECO Chain.',
     description:
       'The #1 AMM and yield farm on Huobi ECO Chain.',
-    launchDate: 'xx xx',
-    launchTime: 'xx xx',
-    saleAmount: 'XXX OYT',
-    raiseAmount: '$XXX',
-    oytToBurn: '$XXX',
+    launchDate: 'Mon Apr 19 2021',
+    launchTime: '0:30 AM UTC',
+    saleAmount: '3000000 OYT',
+    raiseAmount: '$533333.33',
+    oytToBurn: '50%',
     projectSiteUrl: 'https://ogeeswap.com/',
     currency: 'OGE-HT LP',
-    currencyAddress: '',
+    currencyAddress: '0xf7e522999cA7F1306D4df1B5eFcCFAB6A1eD99d6',
     tokenDecimals: 18,
     tokenSymbol: 'OYT',
-    releaseBlockNumber: 5493919,
+    releaseBlockNumber: 3967480,
     campaignId: '00001',
   },
 ]
