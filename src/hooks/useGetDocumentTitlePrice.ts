4c2669d293c26d1478c286567f66d0f5167756e5
JK Reddy <imjaireddy@gmail.com>
2021-04-08 09:54:16 +0530

    title price

--- src/hooks/useGetDocumentTitlePrice.ts
+++ src/hooks/useGetDocumentTitlePrice.ts
@@ -4,10 +4,10 @@ import { usePriceOytHusd } from 'state/hooks'
 const useGetDocumentTitlePrice = () => {
   const oytPriceUsd = usePriceOytHusd()
   useEffect(() => {
-    document.title = `Ogeeswap | Best AMM Platform on Houbi Eco Chain - $${Number(oytPriceUsd).toLocaleString(undefined, {
+    document.title = `Ogeeswap - $${Number(oytPriceUsd).toLocaleString(undefined, {
       minimumFractionDigits: 3,
       maximumFractionDigits: 3,
-    })}`
+    })} | Best AMM Platform on Houbi Eco Chain`
   })
 }
 export default useGetDocumentTitlePrice
